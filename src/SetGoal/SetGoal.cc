c965e33d18a45e770a63350a5f8619982529ba90
mywisdomfly <943167431@qq.com>
2020-06-08 21:47:20 +0800

    exp3 2.3.3

--- src/SetGoal/SetGoal.cc
+++ src/SetGoal/SetGoal.cc
@@ -19,17 +19,18 @@
 // TODO 补充消息头文件
 // #include <____>
 #include "move_base_msgs/MoveBaseActionGoal.h"
+#include "move_base_msgs/MoveBaseActionResult.h"
 
 // 节点基类
 #include "ExperNodeBase.hpp"
 #include "csignal"
 
 /* ========================================== 宏定义 =========================================== */
-#define MACRO_GOAL_POSE_TOPIC   "/move_base/goal"       // 发送导航目标点的 topic
+#define MACRO_GOAL_POSE_TOPIC "/move_base/goal" // 发送导航目标点的 topic
 // TODO 2.3.2 填写你选择的 topic
-// #define MACRO_RESULT_TOPIC      "______"             // 获取导航结果的 topic
+#define MACRO_RESULT_TOPIC "move_base/result" // 获取导航结果的 topic
 
-#define CONST_PI                3.141592654f            // 圆周率
+#define CONST_PI 3.141592654f // 圆周率
 
 /* ========================================== 程序正文 =========================================== */
 using namespace std;
@@ -56,15 +57,15 @@ public:
      * @param[in] ppcArgv       命令行参数表列
      * @param[in] pcNodeName    当前节点运行时的名字
      */
-    SetGoalNode(int nArgc, char** ppcArgv, const char* pcNodeName)
+    SetGoalNode(int nArgc, char **ppcArgv, const char *pcNodeName)
         : ExperNodeBase(nArgc, ppcArgv, pcNodeName)
     {
 
         // TODO 2.3.1 设置发布器
-         mPubNextGoal = mupNodeHandle->advertise<move_base_msgs::MoveBaseActionGoal>(MACRO_GOAL_POSE_TOPIC, 1);
+        mPubNextGoal = mupNodeHandle->advertise<move_base_msgs::MoveBaseActionGoal>(MACRO_GOAL_POSE_TOPIC, 1);
         // TODO 2.3.2 设置导航状态订阅器. 注意回调函数是类的成员函数, 或者是类的静态函数时, 后面应该怎么写; 方式不唯一
-        // mSubNavRes   = _______________(MACRO_RESULT_TOPIC, 1, _____________________);
-
+        mSubNavRes = mupNodeHandle->subscribe(MACRO_RESULT_TOPIC, 1, &SetGoalNode::resCallback,this);
+        isNextpoint = true;
         // 确保初始化完成, 不然可能存在第一条消息发送不出去的情况
         ros::Duration(0.1).sleep();
     }
@@ -77,31 +78,50 @@ public:
     {
         // 2.3.1 发布导航目标点
         // 如果需要自己添加类成员变量或成员函数, 请随意添加
+        uint32_t nTimeCount;
         //等待movebase就位
-        while(mPubNextGoal.getNumSubscribers()==0)
+        while (mPubNextGoal.getNumSubscribers() == 0)
         {
             ros::Duration(1).sleep();
         }
         ros::Duration(1).sleep();
-        while(ros::ok())
+        while (ros::ok())
         {
-            ROS_INFO("Welcome using SetGoal");
-            std::cout<<"Please input the traget point (x,y,yaw) like x y yaw"<<std::endl;
-            double x,y,yaw;
-            do
+            if (isNextpoint)
             {
-                std::cin>>x>>y>>yaw;
-                if(std::cin.fail())
+
+                ROS_INFO("Welcome using SetGoal");
+                std::cout << "Please input the traget point (x,y,yaw) like x y yaw" << std::endl;
+                double x, y, yaw;
+                do
                 {
-                    //cout<<"Input error!"<<endl;
-                    ROS_ERROR("Input error! \n Please input the traget point (x,y,yaw) like x y yaw");
-                    std::cin.clear();
-                    std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+                    std::cin >> x >> y >> yaw;
+                    if (std::cin.fail())
+                    {
+                        //cout<<"Input error!"<<endl;
+                        ROS_ERROR("Input error! \n Please input the traget point (x,y,yaw) like x y yaw");
+                        std::cin.clear();
+                        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                    }
+                    else
+                        break;
+                } while (true);
+                SetCurrGoal(x, y, yaw);
+                nTimeCount=0;
+            }
+            else
+            {
+                if(nTimeCount % 10 == 0)
+                {
+                    ROS_INFO("I am executing!");
                 }
-                else 
-                break;
-            }while(true);
-            SetCurrGoal(x,y,yaw);
+                nTimeCount++;//就是为了延时
+            }
+            
+            ros::Duration(0.5).sleep();
+            
+            ros::spinOnce();
+
         }
 
         // TODO 2.3.2 获取导航执行结果, 并显示在屏幕上
@@ -109,14 +129,15 @@ public:
     }
 
     // TODO 2.3.2 导航执行结果的回调函数
-    /* _______________
+    void resCallback(const move_base_msgs::MoveBaseActionResult::ConstPtr &msg)
     {
-       <YOUR CODE>
+        if (msg->status.status == msg->status.SUCCEEDED)
+        {
+            isNextpoint = true;
+        }
     }
-    */
 
 private:
-
     /**
      * @brief 根据传入坐标生成路径点
      * @param[in] dX            坐标X
@@ -125,17 +146,17 @@ private:
      */
     void SetCurrGoal(double dX, double dY, double dYawDeg)
     {
-        auto& msgHeader         = mMsgCurrGoal.header;
-        auto& msgGoalID         = mMsgCurrGoal.goal_id;
-        auto& msgTargetHeader   = mMsgCurrGoal.goal.target_pose.header;
-        auto& msgPt             = mMsgCurrGoal.goal.target_pose.pose.position;
-        auto& msgQt             = mMsgCurrGoal.goal.target_pose.pose.orientation;
+        auto &msgHeader = mMsgCurrGoal.header;
+        auto &msgGoalID = mMsgCurrGoal.goal_id;
+        auto &msgTargetHeader = mMsgCurrGoal.goal.target_pose.header;
+        auto &msgPt = mMsgCurrGoal.goal.target_pose.pose.position;
+        auto &msgQt = mMsgCurrGoal.goal.target_pose.pose.orientation;
 
         // Step 1 初始化消息头
         msgHeader.seq = nCnt;
         msgHeader.stamp = ros::Time::now();
-        msgHeader.frame_id   = "map";
-        
+        msgHeader.frame_id = "map";
+
         msgGoalID.stamp = msgHeader.stamp;
         msgTargetHeader = msgHeader;
 
@@ -146,20 +167,21 @@ private:
          *      - 修改坐标多发送几次呢?
          *      - 你发现 rviz 中机器人执行导航的过程的不正常情况了吗?
         */
-       ros::Time timeStamp=msgGoalID.stamp;
+        ros::Time timeStamp = msgGoalID.stamp;
         std::stringstream ss;
         ss << "my_goal_" << nCnt << "_" << timeStamp.sec << "." << timeStamp.nsec;
         // 估计是一个hash表，重名的话只保留第一个,发出来的新的不起作用
-        msgGoalID.id = ss.str();// 估计是一个hash表，重名的话只保留第一个,发出来的新的不起作用
+        msgGoalID.id = ss.str(); // 估计是一个hash表，重名的话只保留第一个,发出来的新的不起作用
         ROS_DEBUG_STREAM("Goal Id: " << ss.str());
         Heading2Quat(dYawDeg, msgQt);
-        msgPt.x=dX;
-        msgPt.y=dY;
-        msgPt.z=0;
+        msgPt.x = dX;
+        msgPt.y = dY;
+        msgPt.z = 0;
 
         // Step 2 设置目标点
         // TODO 2.3.1 设置目标点
         mPubNextGoal.publish(mMsgCurrGoal);
+        isNextpoint=false;
         nCnt++;
     }
 
@@ -168,7 +190,7 @@ private:
      * @param[in]  dYawDeg  偏航角, 角度表示
      * @param[out] quat     转换后的四元数
      */
-    void Heading2Quat(double dYawDeg, geometry_msgs::Quaternion& quat)
+    void Heading2Quat(double dYawDeg, geometry_msgs::Quaternion &quat)
     {
         // 实现偏航向四元数的换算
         double dFaiDiv2_rad = 0.5f * dYawDeg / CONST_PI;
@@ -177,18 +199,17 @@ private:
         quat.z = sin(dFaiDiv2_rad);
         quat.w = cos(dFaiDiv2_rad);
     }
-    
-private:
 
+private:
     // TODO 2.3.2 补全类型
     move_base_msgs::MoveBaseActionGoal mMsgCurrGoal;
-    ros::Publisher  mPubNextGoal;           // 路标点发布器
-    ros::Subscriber mSubNavRes;             // 导航状态订阅器
+    ros::Publisher mPubNextGoal; // 路标点发布器
+    ros::Subscriber mSubNavRes;  // 导航状态订阅器
     uint32_t nCnt;
-    // _______________    mMsgCurrGoal;
-    
-};
+    bool isNextpoint;
 
+    // _______________    mMsgCurrGoal;
+};
 
 /**
  * @brief 主函数
@@ -201,8 +222,7 @@ int main(int argc, char **argv)
     // 生成 ROS 节点对象
     SetGoalNode node(argc, argv, "set_goal");
     // 运行
-    signal(SIGINT,OnSignalInterrupt);
+    signal(SIGINT, OnSignalInterrupt);
     node.Run();
     return 0;
 }
-
